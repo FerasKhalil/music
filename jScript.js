3f3ce6ea1f02d439910c9963d64dd8381d943628
Firas Hasan <x.firashasan@gmail.com>
2021-03-22 15:03:07 +0200

    made this commit because i added latest 2 labs..

--- jScript.js
+++ jScript.js
@@ -13,40 +13,71 @@ function func()
     
 }*/
 
-alert("answer this question honestly please!");
+alert("answer these questions honestly please!");
 
 
 var musicVar =prompt("which of these do you prefer most: 'rock, rap, pop, hip hop' ");
+
 var repetition = prompt("how many times would you like the images to repeat?");
-if(musicVar=="rock")
+
+function rockChoice()
 {
     document.write("you prefer rock");
     for(var i =1; i<=repetition; i++){
     document.write('<img src="https://i.pinimg.com/originals/41/e9/95/41e995b504612c384bc3f9e7eda9d29a.jpg"/>');
     }
 }
-else if(musicVar=="rap")
+function rapChoice()
 {
     document.write("you prefer rap");
     for(var i =1; i<=repetition; i++){
     document.write('<img src="https://cdn3.vectorstock.com/i/1000x1000/88/27/inscription-lettering-rap-music-stylized-in-vector-23118827.jpg"/>');
-}}
-else if(musicVar=="pop")
+}
+}
+
+function popChoice()
 {
     document.write("you prefer pop");
     for(var i =1; i<=repetition; i++){
     document.write('<img src="https://pbs.twimg.com/profile_images/1155517544977108992/883LNqWD_400x400.jpg"/>');
-}}
-else if(musicVar=="hip hop")
+}
+}
+
+function hipHopChoice()
 {
     document.write('<center>"you prefer hip hop"</center');
     for(var i =1; i<=repetition; i++){
     document.write('<center> <img src="https://cdn5.vectorstock.com/i/1000x1000/44/49/hip-hop-music-party-in-graffiti-style-vector-23244449.jpg"/></center>');
-}}
-else alert("choose one of the types please!");
+}
+}
+
+
+if(musicVar=="rock")
+{
+    rockChoice();
+}
+
+else if(musicVar=="rap")
+{
+    rapChoice();
+}
+
+else if(musicVar=="pop")
+{
+    popChoice();
+}
+
+else if(musicVar=="hip hop")
+{
+    hipHopChoice();
+}
+
+else { alert("choose one of the types please!"); }
 
 var coloring = confirm("would you like to change the background-color? press ok to submit, press cancel to cancel");
+
 var userColor="";
+
 if(coloring==true)
 {
     userColor = prompt("do you want the background-color to be black, red or blue?");
